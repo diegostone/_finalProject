84cc48f180355c1d6a4e6291cc3657b40732d768
diegostone <38358676+diegostone@users.noreply.github.com>
2018-04-20 09:43:30 -0700

    Update _main.py

--- _main.py
+++ _main.py
@@ -1,3 +1,5 @@
+# https://pastebin.com/MRBDdeku
+
 # ''' I want to use pygame to recreate the  "bob-omb squad" minigame from Super Mario 64 DS. 
 #         deals heavily with x y coordinates to register hits, and a high score point system
 # https://youtu.be/h1FKzGk80Bo?t=842 : this is a video of what the game is 
@@ -21,45 +23,14 @@ import pygame
 import pygame.locals 
 import time 
 import sys
-
-# #images
-
-flower = pygame.image.load("finalresources/images/flower.png")
-
-# # cannonBall = pygame.image.load()
-
-bobOmb = pygame.image.load("finalresources/images/bob-omb.png")
-
-# # gameover = pygame.image.load()
-
-cloud = pygame.image.load("finalresources/images/cloud.png")
-
-# # rope = pygame.image.load()
-
-# #music
+from pygame.sprite import Sprite
 
 
-# # enemy = pygame.mixer.Sound()
-# # drawBack = pygame.mixer.Sound()
-# # shoot = pygame.mixer.Sound()
-
-# # point1 = pygame.mixer.Sound()
-# # point2 = pygame.mixer.Sound()
-# # point3 = pygame.mixer.Sound()
-# # point4 = pygame.mixer.Sound()
-
-# # cloudm = pygame.mixer.Sound()
-
-# # cloudHit = pygame.mixer.Sound()
-
-# # warning = pygame.mixer.Sound()
-
-# # scream = pygame.mixer.Sound()
 #main music
 pygame.mixer.init()
-pygame.mixer.music.load("finalresources/audio/backgroundmusic.mp3")
-pygame.mixer.music.play(-1, 0.0)
-pygame.mixer.music.set_volume(0.25)
+#pygame.mixer.music.load("finalresources/audio/backgroundmusic.mp3")
+#pygame.mixer.music.play(-1, 0.0)
+#pygame.mixer.music.set_volume(0.25)
 
 if pygame.mixer.music.get_busy() == False:
     pygame.mixer.music.rewind("finalresources/audio/backgroundmusic.mp3")
@@ -67,7 +38,7 @@ if pygame.mixer.music.get_busy() == False:
 
 # # #loadimages
 
-
+flower = pygame.image.load("finalresources/images/flower.png")
 background = pygame.image.load("finalresources/images/background.png")
 background = pygame.transform.scale(background, (480, 640))
 flower = pygame.transform.scale(flower, (50,50))
@@ -86,13 +57,39 @@ class Cloud():
         self.rect = self.image.get_rect()
         self.screen.rect = screen.get_rect()
         self.center = float(self.rect.centerx)
-    def update(self):
-        print()
 
+    #def update(self):
+        
     def draw(self):
          self.screen.blit(self.image, self.rect)   
 
+class bobOmb(Sprite):
+    def __init__(self,screen,game_settings,pos,speed,velocity):
+        self.screen = screen
+        self.image = pygame.image.load("finalresources/images/bob-omb.png")
+        self.rect = self.image.get_rect(center=pos)
+        self.speed = speed
+        self.target = None
+        self.position = list(self.rect.center)
+        self.distance = None
+        self.vec = None
+        self.__dict__[self.vec]
+        self.velocity = velocity
+    def moveLoop(self):
+        while True:
+            count += 1
+            if count == 10:
+                count = 0
+                bobOmb.move(0, 3)
+    def location(self):
+        self.x = x
+        self.y = y
+        self.xVelo = -self.velocity
+    def draw(self):
+        self.screen.blit(self.image, self.rect)
+    
 
+      
 
 def run_game():
     #initialize pygame, settings, and screen object
@@ -107,13 +104,15 @@ def run_game():
     pygame.init()
     screen = pygame.display.set_mode((game_settings.screen_width,game_settings.screen_height))
     pygame.display.set_caption("Bob-omb Squad")
-
-
+    #enemy = bobOmb(game_settings, screen)
 
 
     #Start the main loop for the run_game
     while True:
         screen.fill(0)
+        clock = pygame.time.Clock()
+        clock.tick(0.5)
+
         #Watch for keyboard and mouse events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -125,11 +124,8 @@ def run_game():
         screen.blit(flower,(262,565)) 
         screen.blit(flower,(340,565)) 
         pygame.display.update()
+
     #starts timer for spawning
-        timer = 1000
-        timer-=1
-        if timer <= 500:
-            screen.blit(cloud,(10,10))
-            
+        
 
 run_game()
